7d3a2d8b996c50d7245577322090ff375b491f3b
tupm209 <tu.pm209@gmail.com>
2025-09-21 00:06:39 +0900

    Add files via upload

---
+++ hw03/main.js
@@ -0,0 +1,6 @@
+let width = 10;
+let height = 20;
+let area = width*height;
+document.writeln("Chiều rộng là: " + width + "<br>");
+document.writeln("Chiều cao là: " + height + "<br>");
+document.writeln("Diện tích là: " + area);
