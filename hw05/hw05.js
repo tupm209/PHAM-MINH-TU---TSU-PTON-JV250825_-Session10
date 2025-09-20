513eba1f8d25517145b37d3f7252fddba4feaf6a
tupm209 <tu.pm209@gmail.com>
2025-09-21 00:20:56 +0900

    Add files via upload

---
+++ hw05/hw05.js
@@ -0,0 +1,6 @@
+let usDollar = Number(prompt("Hãy nhập số tiền Đô la Mỹ muốn chuyển đổi: "));
+
+let vnDong = usDollar*25000;
+
+document.writeln("Tỷ giá hiện tại 1$ = 25.000đ.<br>");
+document.writeln("Giá VNĐ sau khi quy đổi: " + vnDong + "đ");
