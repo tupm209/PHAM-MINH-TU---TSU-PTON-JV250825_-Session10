a636ec17338486ae51f6f6376a29bc72f9c7f6a6
tupm209 <tu.pm209@gmail.com>
2025-09-21 00:10:44 +0900

    Add files via upload

---
+++ hw04/hw04.js
@@ -0,0 +1,5 @@
+let firstNumber = Number(prompt("Hãy nhập số thứ nhất: "));
+
+let secondNumber = Number(prompt("Hãy nhập số thứ hai: "));
+
+alert("Tổng 2 số là: " + (firstNumber+secondNumber) + "!");
